3bf800f4d770bd4e1f46e54e3b62ba7b8a8672b9
connor <c.stroomberg@gmail.com>
2019-03-06 17:15:01 +0100

    Fix lint errors

--- tests/unit/MosaicView.spec.ts
+++ tests/unit/MosaicView.spec.ts
@@ -1,19 +1,19 @@
-jest.mock('@/repository/ExperimentRepository',  () => ({
+import { shallowMount } from '@vue/test-utils'
+import MosaicView from '@/components/MosaicView.vue'
+jest.mock('@/repository/ExperimentRepository', () => ({
   saveExpData: jest.fn(),
   saveResultFileId: jest.fn(),
   removeData: jest.fn()
 }))
 
-jest.mock('@/repository/ScriptJobRepository',  () => ({
+jest.mock('@/repository/ScriptJobRepository', () => ({
   run: jest.fn(),
   poll: jest.fn()
 }))
-import { shallowMount } from '@vue/test-utils'
-import MosaicView from '@/components/MosaicView.vue'
 
 describe('MosaicView.vue', () => {
   it('creates a component with name MosaicView', () => {
     const wrapper = shallowMount(MosaicView)
-    expect(wrapper.name()).toMatch("MosaicView")
+    expect(wrapper.name()).toMatch('MosaicView')
   })
 })
