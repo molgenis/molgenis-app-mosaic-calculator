960a9508d44ba03c62ba6966c054c203220279c7
connor <c.stroomberg@gmail.com>
2019-03-10 17:22:55 +0100

    Tests for ScriptJobRepository

--- src/repository/ScriptJobRepository.ts
+++ src/repository/ScriptJobRepository.ts
@@ -9,14 +9,14 @@ const poll = (pollId: string) => {
   return api.get('/api/v2/sys_job_ScriptJobExecution/' + pollId)
 }
 
-const run = (id: string) => {
+const run = (experimentId: string) => {
   const options = {
     headers: {
       'Accept': 'text/plain',
       'X-Requested-With': 'XMLHttpRequest'
     }
   }
-  return api.post('/scripts/' + SCRIPT_NAME + '/submit?id=' + id, options).then(
+  return api.post('/scripts/' + SCRIPT_NAME + '/submit?id=' + experimentId, options).then(
     (response: any) => {
       return response.text().then(getIdFromPollUrI)
     }
