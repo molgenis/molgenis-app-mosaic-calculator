c76b11867a222253262fa55a71b4de423f70f431
connor <c.stroomberg@gmail.com>
2019-03-11 15:15:01 +0100

    Cleanup script job using httr instead of molgenisApi

--- scripts/mosaic_cleanup.R
+++ scripts/mosaic_cleanup.R
@@ -1,9 +1,12 @@
-library("molgenisRApi")
 library("rjson")
+library("httr")
 library("RCurl")
 mol.url <- "docker.for.mac.localhost:8080"
+#mol.url <- "http://localhost:8080"
 entity <- "mosaic_exp_data"
-token <- molgenis.login(mol.url, "admin", "admin")
+token <- '${molgenisToken}'
+
+# fetch the rows to be cleaned
 url <- paste0(mol.url, "/api/v2/", entity, "?molgenis-token=", token)
 response <- fromJSON(getURL(url))
 analyses <- response$items
@@ -12,14 +15,14 @@ print("delete analysis data")
 for(analysis in analyses) {
     analysis.id <- analysis$id
     print(analysis.id)
-    molgenis.delete(entity = entity, id = analysis.id)
+    DELETE(paste0(mol.url, "/api/v1/mosaic_exp_data/", analysis.id), add_headers('x-molgenis-token' = token))
 }
 
 print("delete result data")
 for(analysis in analyses) {
     result.file.id <- analysis$resultFileId
     print(result.file.id)
-    molgenis.delete(entity = "sys_FileMeta", id = result.file.id)
+    DELETE(paste0(mol.url, "/api/v1/sys_FileMeta/", result.file.id), add_headers('x-molgenis-token' = token))
 }
 
 print("clean up done")
